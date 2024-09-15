7a5cb20f5dd58c4dbbfee90d0899dfaf3ae19fbb
UniversoParalelo <cmartinrodich@gmail.com>
2024-09-15 20:07:14 -0300

    Iniciando repo

---
+++ main.py
@@ -0,0 +1,118 @@
+import json
+import time
+
+QUANTO = 3
+
+
+def round_robin(procesos, quanto=3):
+    cronograma_falso = dict()
+    cronograma = dict()
+    no_times = 0
+    time = 0
+
+    while no_times < 10:
+        for proceso in procesos:
+            if (proceso["tiempo_arribo"] <= time) and (proceso['tiempo_irrupcion'] > 0):
+                if proceso['tiempo_irrupcion'] - quanto <= 0:
+                    time += proceso['tiempo_irrupcion']
+                    proceso['tiempo_irrupcion'] = 0
+                    no_times += 1
+                else:
+                    proceso["tiempo_irrupcion"] -= quanto
+                    time += quanto
+
+                cronograma_falso[time] = proceso["proceso_id"]
+    
+    data = getData()
+    for tiempo, nombre_proceso in cronograma_falso.items():
+        for proceso in data:
+            if (nombre_proceso==proceso['proceso_id']):
+                cronograma[tiempo] = proceso
+
+    return cronograma
+
+
+def worst_fit(cola_listos, mp, finalizados):
+    max_espacio_libre = 0
+    max_indice = 0
+
+    for key,value in mp.items():
+        if value['espacio_ocupado']==0:
+            espacio_libre = value['espacio_libre']-cola_listos[0]['tamanio']
+            if espacio_libre > max_espacio_libre:
+                max_espacio_libre = espacio_libre
+                max_indice = key
+    
+    if max_indice != 0:
+        mp[max_indice]['espacio_libre'] = max_espacio_libre
+        mp[max_indice]['espacio_ocupado'] = cola_listos[0]['tamanio']
+        finalizados.append(cola_listos[0])
+        cola_listos.pop(0)
+        # print(mp)
+       # print(cola_listos)
+        
+    return [max_espacio_libre, max_indice]
+
+def imprimir():
+    pass
+
+def calcular_tiempos():
+    pass
+
+def procesador(cronograma):
+    cola_listos = cronograma[:5] # agrego primero cinco procesos
+    cola_suspendidos = []
+    finalizados = []
+    mp = { 
+       1: {
+        'espacio_ocupado': 0,
+        'espacio_libre': 250000
+    }, 2: {
+        'espacio_ocupado': 0,
+        'espacio_libre': 150000
+    }, 3: {
+        'espacio_ocupado': 0,
+        'espacio_libre': 50000
+    }}
+    fr_int = []
+    fr_ext = []
+    tiempo = cronograma[0].tiempo_arribo
+    proceso_actual = cronograma[0].proceso_id
+
+    
+    while len(finalizados) != len(cronograma):
+
+        while cola_listos['tiempo_arribo'] <= tiempo:
+            cola_listos.append(cronograma[5+len(finalizados)])
+            resultados = worst_fit(cola_listos, mp, finalizados)
+
+            if resultados[1] == 0:
+                break
+
+        
+
+
+        
+        
+        if proceso_actual != '':
+            print(proceso_actual["proceso_id"], tiempo)
+            finalizados.append(cronograma[len(finalizados)])
+        else:
+            tiempo += QUANTO
+        
+
+
+
+def getData():
+    # Leer el JSON desde un archivo
+    with open('procesos.json', 'r') as file:
+        return json.load(file)
+
+
+data = getData()
+
+cronograma = round_robin(data)
+print(cronograma)
+# procesador(cronograma)
+
+

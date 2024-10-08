aa3b74a8c804a27e6febcd9a9267bc1136185f40
UniversoParalelo <cmartinrodich@gmail.com>
2024-10-08 09:34:21 -0300

    primer version del simulador completa

--- main.py
+++ main.py
@@ -75,34 +75,36 @@ def asignarMemoria(cronograma):
     cola_listos = []
     cola_listos_bloqueados = []
     cola_terminados = []
-    tiempo_general = claves[i]
+    tiempo_general = claves[0]
+    ultimo_valor = 0
 
-    while len(cronograma) > 0:
+    while (len(cola_terminados) < 10):
         print('====================================================================')
         print('Tiempo general: ', tiempo_general)
         proceso_en_ejecucion = cronograma[claves[i]]
 
         # agregamos procesos a cola de listos o a cola de listos y bloqueados
-        j = 0
-        for j in range(10):
+        for j in range(ultimo_valor, len(cronograma)):
             if cronograma[claves[j]]['estado']=='nuevo' and cronograma[claves[j]]['tiempo_arribo'] <= tiempo_general:
                 if len(cola_listos) < 3:
                     cola_listos.append(cronograma[claves[j]])
                     cronograma[claves[j]]['estado'] = 'listo'
+                    ultimo_valor += 1
                     
-                elif len(cola_listos_bloqueados) < 2:
-                    cola_listos_bloqueados.append(cronograma[claves[j]])
-                    cronograma[claves[j]]['estado'] = 'bloqueado'
+                # elif len(cola_listos_bloqueados) < 2:
+                #     cola_listos_bloqueados.append(cronograma[claves[j]])
+                #     cronograma[claves[j]]['estado'] = 'bloqueado'
+                #     ultimo_valor += 1
 
             else:
                 break
 
         
         # nos fijamos si el primer proceso puede ser asignado a una particion en memoria
+        # voy a repetir codigo, optimizar mas adelante !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         while len(cola_listos) > 0:
             asignado = worstFit(cola_listos[0])
             if asignado == 0:
-            # fijarse si es un proceso que debe ser ejecutado 
                 break
             else:
                 MEMORIA_PRINCIPAL[asignado]['proceso'] = cola_listos[0]['proceso_id']
@@ -115,21 +117,21 @@ def asignarMemoria(cronograma):
         print('Cola de terminados: ', cola_terminados)
 
         # chequeamos que el proceso en ejecucion este asignado en memoria
-        
         esta_en_mp = False
         for particion in MEMORIA_PRINCIPAL:
             if MEMORIA_PRINCIPAL[particion]['proceso'] == proceso_en_ejecucion['proceso_id']:
                 esta_en_mp = True
                 MEMORIA_PRINCIPAL[particion]['estado'] = 'libre'
                 MEMORIA_PRINCIPAL[particion]['proceso'] = 0
+                break
         
+        print('Proceso en ejecucion: ', proceso_en_ejecucion['proceso_id'])
         if esta_en_mp:
-            print('Proceso en ejecucion: ', proceso_en_ejecucion['proceso_id'])
             i += 1
             if proceso_en_ejecucion['tiempo_irrupcion'] - QUANTO <= 0:
                 cola_terminados.append(proceso_en_ejecucion['proceso_id'])
                 tiempo_general += proceso_en_ejecucion['tiempo_irrupcion']
-                # del cronograma[claves[i]]
+                # del cronograma[claves[0]]
             else:
                 tiempo_general += QUANTO
         

39bbf09c3fe1fed6bb0da71060a1ede0348fc8b7
UniversoParalelo <cmartinrodich@gmail.com>
2024-11-05 13:22:56 -0300

    Arreglo al codigo. Bug de procesos_ordenados

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import time
 import csv
 
 # Constantes
@@ -31,6 +30,7 @@ def leer_json(json_file_path):
         data = json.load(json_file)
     return data
 
+
 def leer_csv(csv_file_path):
     data = []
     with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
@@ -38,6 +38,7 @@ def leer_csv(csv_file_path):
         for row in csv_reader:
             data.append(row)
     return data
+
 
 def obtener_datos(file_path):
     if file_path.endswith('.json'):
@@ -47,87 +48,36 @@ def obtener_datos(file_path):
     else:
         raise ValueError("Formato de archivo no soportado. Usa JSON o CSV.")
 
+
 def ordenar_datos(datos):
     return sorted(datos, key=lambda x: x['tiempo_arribo'])
 
-# def crearCronogramaRR(datos_ordenados):
-#     tiempo_general = 0
-#     var_corte = 0 
-#     cronograma = {}
-
-#     while var_corte < len(datos_ordenados):
-        
-#         for proceso in datos_ordenados:
-#             if proceso['tiempo_arribo'] <= tiempo_general:
-#                 if proceso['tiempo_irrupcion'] != 0:
-#                     cronograma[tiempo_general] = proceso.copy()
-#                     if proceso['tiempo_irrupcion'] <= QUANTO:
-#                         tiempo_general += proceso['tiempo_irrupcion']
-#                         proceso['tiempo_irrupcion'] = 0
-#                         var_corte += 1
-#                     else:
-#                         proceso['tiempo_irrupcion'] -= QUANTO
-#                         tiempo_general += QUANTO    
-    # return cronograma
 
 def asignarMemoria(procesos_ordenados):
-    # claves = list(procesos_ordenados)
-    i = 0
     cola_listos = []
     cola_listos_suspendidos = []
     cola_terminados = []
     tiempo_general = 0
     tiempos = []
 
-    while (len(cola_terminados) < 10):
+    # agregamos procesos a cola de listos o a cola de listos y suspendidos
+    agregarColaListos(procesos_ordenados, cola_listos, cola_listos_suspendidos, tiempo_general)
+    
 
-        # agregamos procesos a cola de listos o a cola de listos y suspendidos
-        for proceso in procesos_ordenados:
-            if proceso['estado']=='nuevo' and proceso['tiempo_arribo'] <= tiempo_general:
-                if len(cola_listos) < 3:
-                    asignado = worstFit(proceso)
-                    if asignado == 0:
-                        if len(cola_listos_suspendidos) < 2:
-                            cola_listos_suspendidos.append(proceso)
-                            proceso['estado'] = 'suspendido'
-                    else:
-                        cola_listos.append(proceso)
-                        proceso['estado'] = 'listo'
-                        MEMORIA_PRINCIPAL[asignado]['proceso'] = proceso['proceso_id']
-                        MEMORIA_PRINCIPAL[asignado]['fr_interna'] = MEMORIA_PRINCIPAL[asignado]['tamanio'] - proceso['tamanio']
-                        MEMORIA_PRINCIPAL[asignado]['estado'] = 'ocupado'
-                       
-                elif len(cola_listos_suspendidos) < 2:
-                    cola_listos_suspendidos.append(proceso)
-                    proceso['estado'] = 'suspendido'
-
-            else:
-                break
-        
-        # sacar procesos de procesos_ordenados que ya estan en cola_listos y cola_listos_suspendidos
-        for proceso in procesos_ordenados:
-            if proceso in cola_listos or proceso in cola_listos_suspendidos:
-                procesos_ordenados.remove(proceso)
-
-
+    while (len(cola_listos) > 0):
         # ejecutar procesos en cola_listos
         for proceso in cola_listos:
-            print('====================================================================')
-            print('Tiempo general: ', tiempo_general)
-            print('Cola de listos: ', cola_listos)
-            print('Cola de listos suspendidos: ', cola_listos_suspendidos)
-            imprimirMP()
-            print('Cola de terminados: ', cola_terminados)
-            print('Proceso en ejecucion: ', proceso['proceso_id'])
-            print('====================================================================')
+            impresionParcial(tiempo_general, cola_listos, cola_listos_suspendidos, cola_terminados, proceso)
 
+            # el proceso ya termino de ejecutarse entonces liberamos memoria y lo pasamos a cola de terminados
+            # aniadimos un proceso de la cola de listos suspendidos a la cola de listos SI ES POSIBLE
             if proceso['tiempo_irrupcion'] <= QUANTO:
                 tiempo_general += proceso['tiempo_irrupcion']
                 tiempo_retorno = tiempo_general - proceso['tiempo_arribo']
                 tiempos.append({
                     'proceso': proceso['proceso_id'],
                     'tiempo_retorno': tiempo_retorno,
-                    'tiempo_espera': tiempo_retorno - proceso['tiempo_irrupcion']
+                    'tiempo_espera': tiempo_retorno - proceso['tiempo_irrupcion_constante']
                 })
                 # buscar particion del proceso
                 particion = 0
@@ -145,27 +95,63 @@ def asignarMemoria(procesos_ordenados):
 
                 # pregunto por la cola de listos suspendidos
                 if len(cola_listos_suspendidos) > 0:
-                    nuevo_proceso = cola_listos_suspendidos[0]
-                    asignado = worstFit(nuevo_proceso)
-                    if asignado == 0:
-                        continue
-                    else:
-                        cola_listos.append(nuevo_proceso)
-                        nuevo_proceso['estado'] = 'listo'
-                        MEMORIA_PRINCIPAL[asignado]['proceso'] = nuevo_proceso['proceso_id']
-                        MEMORIA_PRINCIPAL[asignado]['fr_interna'] = MEMORIA_PRINCIPAL[asignado]['tamanio'] - nuevo_proceso['tamanio']
-                        MEMORIA_PRINCIPAL[asignado]['estado'] = 'ocupado'
-                        print(cola_listos_suspendidos, proceso)
-                        cola_listos_suspendidos.remove(nuevo_proceso)
+                    for proceso2 in cola_listos_suspendidos:
+                        asignado = worstFit(proceso2)
+                        if asignado != 0:
+                            cola_listos.append(proceso2)
+                            cola_listos_suspendidos.remove(proceso2)
+                            proceso2['estado'] = 'listo'
+                            MEMORIA_PRINCIPAL[asignado]['proceso'] = proceso2['proceso_id']
+                            MEMORIA_PRINCIPAL[asignado]['fr_interna'] = MEMORIA_PRINCIPAL[asignado]['tamanio'] - proceso2['tamanio']
+                            MEMORIA_PRINCIPAL[asignado]['estado'] = 'ocupado'
+                            break
 
             else:
                 tiempo_general += QUANTO
                 proceso['tiempo_irrupcion'] -= QUANTO
-
+                
+            agregarColaListos(procesos_ordenados, cola_listos, cola_listos_suspendidos, tiempo_general)
         
-        input('Presiona enter para continuar...')
     imprimirTiempos(tiempos, tiempo_general)
-        
+
+
+def limpiarProcesosOrdenados(procesos_ordenados, cola_listos, cola_listos_suspendidos):
+    i = 0
+
+    while i < len(procesos_ordenados):
+        if procesos_ordenados[i] in cola_listos or procesos_ordenados[i] in cola_listos_suspendidos:
+            procesos_ordenados.remove(procesos_ordenados[i])
+        else:
+            i += 1
+
+
+def agregarColaListos(procesos_ordenados, cola_listos, cola_listos_suspendidos, tiempo_general):
+
+    for proceso in procesos_ordenados:
+        if proceso['estado']=='nuevo' and proceso['tiempo_arribo'] <= tiempo_general:
+            if len(cola_listos) < 3:
+                asignado = worstFit(proceso)
+                if asignado == 0:
+                    if len(cola_listos_suspendidos) < 2:
+                        cola_listos_suspendidos.append(proceso)
+                        proceso['estado'] = 'suspendido'
+                else:
+                    cola_listos.append(proceso)
+                    proceso['estado'] = 'listo'
+                    MEMORIA_PRINCIPAL[asignado]['proceso'] = proceso['proceso_id']
+                    MEMORIA_PRINCIPAL[asignado]['fr_interna'] = MEMORIA_PRINCIPAL[asignado]['tamanio'] - proceso['tamanio']
+                    MEMORIA_PRINCIPAL[asignado]['estado'] = 'ocupado'
+                    
+            elif len(cola_listos_suspendidos) < 2:
+                cola_listos_suspendidos.append(proceso)
+                proceso['estado'] = 'suspendido'
+
+        else:
+            break
+
+    # sacar procesos de procesos_ordenados que ya estan en cola_listos y cola_listos_suspendidos
+    limpiarProcesosOrdenados(procesos_ordenados, cola_listos, cola_listos_suspendidos)
+
 
 def worstFit(proceso):
     tamanio_proceso = proceso['tamanio']
@@ -179,10 +165,33 @@ def worstFit(proceso):
             
     return particion
 
+
+def impresionParcial(tiempo_general, cola_listos, cola_listos_suspendidos, cola_terminados, proceso):
+    print('====================================================================')
+    print('Tiempo general: ', tiempo_general)
+    print('Cola de listos: ', end="")
+    for proceso2 in cola_listos: print(proceso2['proceso_id'], end=" ")
+    print('\nCola de listos suspendidos: ', end="")
+    for proceso3 in cola_listos_suspendidos: print(proceso3['proceso_id'], end=" ")
+    imprimirMP()
+    print('\nCola de terminados: ', end="")
+    for proceso4 in cola_terminados: print(proceso4['proceso_id'], end=" ")
+    print('Proceso en ejecucion: ', proceso['proceso_id'])
+    print('====================================================================')
+    input('Presiona enter para continuar...')
+
+
 def imprimirMP():
-    print('Memoria principal: ')
+    print('\nMemoria principal: ')
+    valores = MEMORIA_PRINCIPAL[1].keys()
+    for valor in valores:
+        print(valor, end=" | ")
+    
     for key, value in MEMORIA_PRINCIPAL.items():
-        print(key, value)
+        print(f"\n{key}", end=" | ")
+        for v in value.values():
+            print(v, end=" | ")
+
 
 def imprimirTiempos(tiempos, tiempo_total):
     print('------------------------------------------------------------')
@@ -198,6 +207,7 @@ def imprimirTiempos(tiempos, tiempo_total):
     rendimiento = len(tiempos) / tiempo_total
     print('Rendimiento: ', rendimiento)
     print('--------------------------------------------------------------')
+
 
 def imprimirDatos(datos):
     print('Procesos: ')
